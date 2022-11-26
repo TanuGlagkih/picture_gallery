77ec00e6d7c1cd5a92b41a20003a0149e42686ba
Tanu Gladkih <tat-glad@yandex.ru>
2022-11-26 22:11:19 +0300

    fix: imports were fixed~

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { TRootState } from '../../services/config-store';
 import Picture from '../picture/Picture';
+
 import styles from './showcase.module.css'
 
 export default function Showcase() {
