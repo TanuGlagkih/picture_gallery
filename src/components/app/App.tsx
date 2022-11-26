b6e96e2bbe49588e556f83bf3ae515a26756d6d8
Tanu Gladkih <tat-glad@yandex.ru>
2022-11-26 21:18:01 +0300

    fix: imports were fixed

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { pictures } from '../../assets/data/data';
 import { fillCart, fillShowcase } from '../../services/main-store';
-import Footer from '../footer/footer';
-import Header from '../header/header';
-import Showcase from '../showcase/showcase';
+import Footer from '../footer/Footer';
+import Header from '../header/Header';
+import Showcase from '../showcase/Showcase';
 
 export default function App() {
     const dispatch = useDispatch();
